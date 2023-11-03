5864240b73d12e7fe84fe4c5632e8d25c180d40a
ZuzanaElt <zuzanaeltom@gmail.com>
2023-11-03 10:52:31 +0000

    added event listener and alert

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const button = document.getElementById("button");
-// alert('hello world');
+const applicationForm = document.getElementById("applicationForm")
+const firstName = document.getElementById("firstName")
+const surname = document.getElementById("surname")
+const email = document.getElementById("email")
 
-// button.addEventListener("mouseover", function changeColor(){
-//     button.style.backgroundColor="orange";
-// });
 
-// button.addEventListener("mouseout", function changeColor(){
-//     button.style.backgroundColor="blue";
-// });
+button.addEventListener("mouseover", function changeColor(){
+    button.style.backgroundColor="orange";
+});
 
-button.addEventListener("click",function showResponse(){
-    console.log('hello world');
+button.addEventListener("mouseout", function changeColor(){
+    button.style.backgroundColor="blue";
+});
+
+
+
+button.addEventListener("click",function(e){
+       //stops deleting the form details after click
+    //    e.preventDefault()
+    
+    if(surname.value !== "" && email.value !== "" && linkedin.value !=="" ){
+        alert('Thank you for submitting your Application')
+    }
 }
-);
+)
+
+;
+
 
