0acfdf630b3c51a61a731182f08007cbfa093fe0
ZuzanaElt <zuzanaeltom@gmail.com>
2023-11-03 09:21:47 +0000

    html, css first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("button");
+// alert('hello world');
+
+// button.addEventListener("mouseover", function changeColor(){
+//     button.style.backgroundColor="orange";
+// });
+
+// button.addEventListener("mouseout", function changeColor(){
+//     button.style.backgroundColor="blue";
+// });
+
+button.addEventListener("click",function showResponse(){
+    console.log('hello world');
+}
+);
+
